43d44ba9a02b859805d320aed05fe132ee99a21a
yazbrielmendoza <yazbriel.mendoza@escola.pr.gov.br>
2025-05-28 09:05:53 -0300

    28;5

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'Quando començou a Segunda Guerra Mundial?',
+    'A segunda Guerra Mundial començou o 1º de setembro de 1939'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Emiratos Arabes Unidos?',
+    'A capital dos Emiratos Arabes Unidos é Abu Dabi '
+)
+
+criaCartao(
+    'biologia',
+    'O que são as mitocondrias ?',
+    'é uma organela presente na maioria das células eucarióticas, conhecida como "usina de energia" da célula.'
+)
+
+criaCartao(
+    'Lingua Espanhol',
+    'Como se diz Bem-vindo em Espanhol?',
+    'se diz BIENVENIDOS'
+)
